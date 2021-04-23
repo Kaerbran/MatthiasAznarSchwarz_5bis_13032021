7e7a3756bdb0fb6d0e4066d50793de63b0ad4b69
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-04-23 22:09:20 +0200

    premier regex fonctionne

--- pageFillingPanier.js
+++ pageFillingPanier.js
@@ -57,7 +57,7 @@ const getTeddy = async function (APIUrl) {
 
 // --------------------------------------------------------------------------------
 
-//permet de connaitre la liste de course
+//permet d'afficher la liste de course
 var basketToDisplay = JSON.parse(sessionStorage.listeBasketTest3);
 //le tableau est completer en fonction des élements qui se trouvent dans l'object "panier"
 for (let index = 0; index < basketToDisplay.length; index++) {
@@ -84,31 +84,70 @@ for (let index = 0; index < basketToDisplay.length; index++) {
         secondElem.appendChild(newTdElemPrice);
 }  
 
+//boucle pour calculer la somme du panier
 var sommePrix = 0;
 for (let index = 0; index < basketToDisplay.length; index++) {
     sommePrix += Number(basketToDisplay[index].price);
 }
 document.getElementById('summePriceBasket').innerHTML = sommePrix;
 
-const sendPurchaseOrder = function (element) {
+var firstNameVerification = false;
+var lastNameVerification = true;
+var adressVerification = true;
+var cityVerification = true;
+var emailVerification = true;
+
+//EventListener pour s'assurer que la valeur des champs est juste
+document.getElementById('firstName').addEventListener('change', function (event) {
+  let constraint = /^[ a-zA-Z\-\’]{3,}/;
+
+    if (constraint.test(this.value)) { //ça fonctionne :)
+        firstNameVerification = true;
+        if (firstNameVerification + lastNameVerification + adressVerification + cityVerification + emailVerification) {
+            document.getElementById("bttFormSend").removeAttribute("disabled");
+        }
+    }
+    else { //ne fonctionne pas :(
+        document.getElementById("bttFormSend").setAttribute("disabled", true);
+        firstNameVerification = false;
+    }
+} );
+document.getElementById('lastName').addEventListener('change', function () {
+    
+});
+document.getElementById('adress').addEventListener('change', function () {
+    
+});
+document.getElementById('city').addEventListener('change', function () {
+    
+});
+document.getElementById('email').addEventListener('change', function () {
+    
+});
+
+//eventListener pour envoyer le formulaire
+document.getElementById('bttFormSend').addEventListener('click', function (event) {
+    event.preventDefault();
+
     let formPurchaseOrder = {
         contactDetails : {
-            firstName : document.getElementById('firstName').innerHTML,
-            lastName : document.getElementById('lastName').innerHTML,
-            adresse : document.getElementById('adress').innerHTML,
-            city : document.getElementById('city').innerHTML,
-            email : document.getElementById('email').innerHTML},
+            firstName : document.getElementById('firstName').value,
+            lastName : document.getElementById('lastName').value,
+            adresse : document.getElementById('adress').value,
+            city : document.getElementById('city').value,
+            email : document.getElementById('email').value},
         Basket : []
     }
 
     for (let index = 0; index < basketToDisplay.length; index++) {
         let productAddForm = {
             id : basketToDisplay[index].id,
-            color basketToDisplay[index].color: 
+            color : basketToDisplay[index].color 
         }
         formPurchaseOrder.Basket.push(productAddForm);
     }
 
-    //ici mettre Fetch with POST
-    
-}
+    console.log(formPurchaseOrder);
+
+    //ici mettre Fetch with POST   
+});
